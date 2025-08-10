62e8ef7d76f8848786b37e7359123d7c3c5ca754
rsrax <rs.wolviepool1499@gmail.com>
2025-08-10 21:11:19 +0100

    Implement OpenMLS PQC Crypto Provider (a replica of default OpenMLS Rust Crypto Provider)  with dependencies and features

--- openmls_pqc_crypto/src/lib.rs
+++ openmls_pqc_crypto/src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! # OpenMLS Custom PQC Capable Crypto Provider
+//!
+//! This is an implementation of the [`OpenMlsProvider`] trait to use with
+//! OpenMLS.
+
+pub use openmls_memory_storage::{MemoryStorage, MemoryStorageError};
+use openmls_traits::OpenMlsProvider;
+
+mod provider;
+pub use provider::*;
+
+#[derive(Default, Debug)]
+#[cfg_attr(feature = "test-utils", derive(Clone))]
+pub struct OpenMlsPqcProvider {
+    crypto: PqcCrypto,
+    key_store: MemoryStorage,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl OpenMlsProvider for OpenMlsPqcProvider {
+    type CryptoProvider = PqcCrypto;
+    type RandProvider = PqcCrypto;
+    type StorageProvider = MemoryStorage;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn storage(&self) -> &Self::StorageProvider {
+        &self.key_store
+    }
+
+    fn crypto(&self) -> &Self::CryptoProvider {
+        &self.crypto
+    }
+
+    fn rand(&self) -> &Self::RandProvider {
+        &self.crypto
     }
 }
